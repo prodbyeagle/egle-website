8c5b548cf74fcb030ab636cb82258355b2c89dc7
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-06-26 23:33:50 +0200

    Hotfix for Updates

--- src/updates.js
+++ src/updates.js
@@ -1,19 +1,13 @@
 // updates.js
 export const updates = [
    {
-      version: "0.6",
+      version: "0.6.1",
       date: "26.06.2024",
       changes: [
          "Added that you can Click the Timer",
-         "Added What's New!"
-      ]
-   },
-   {
-      version: "0.5",
-      date: "26.06.2024",
-      changes: [
-         "Added that you can Click the Timer",
-         "Added What's New!"
+         "Added What's New!",
+         "Made the Requirements an Section instead of an Modal",
+         "Fixed the Padding of the Layout"
       ]
    }
 ];
